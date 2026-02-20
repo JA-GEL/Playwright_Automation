fd3aa2d69d5e43c4e2deb170ad89b2965a7a123e
Jo <jgeling22@gmail.com>
2026-02-21 02:45:04 +0800

    Update

--- tests/My Assessment/Assessment QA.spec.js
+++ tests/My Assessment/Assessment QA.spec.js
@@ -1,6 +1,6 @@
-import { test, expect } from '@playwright/test';
+import { test, expect} from '@playwright/test';
 
-test('Login Sucessfully', async ({ page }) => {
+test('Login Sucessfully & Checkout', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
   await expect(page).toHaveTitle(/Swag Labs/);
 
